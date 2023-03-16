11bc0a9eaa33a8f0ca95fe7e07371499942d74b5
flazepe <flazepe@gmail.com>
2023-03-16 19:32:23 +0800

    refactor: import `AeonClient` directly

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-use anyhow::Result;
-use slashook::main;
-
 pub mod commands;
 pub mod constants;
-pub mod structs;
 pub mod macros;
+pub mod structs;
 pub mod traits;
+
+use anyhow::Result;
+use slashook::main;
+use structs::client::AeonClient;
 
 #[main]
 async fn main() -> Result<()> {
-    let mut client = structs::client::AeonClient::new()?;
+    let mut client = AeonClient::new()?;
 
     client.register_commands().await?;
     client.start().await;
