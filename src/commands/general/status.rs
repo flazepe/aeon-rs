f1251d2f691b43dd8e21fa4864c53acbb499781d
flazepe <flazepe@gmail.com>
2023-03-26 17:48:56 +0800

    refactor: change to embed response

--- src/commands/general/status.rs
+++ src/commands/general/status.rs
@@ -1,6 +1,10 @@
-use crate::{statics::emojis::*, *};
+use crate::{
+    statics::{colors::*, emojis::*},
+    *,
+};
 use anyhow::Context;
 use slashook::commands::{CommandInput, CommandResponder};
+use slashook::structs::embeds::Embed;
 use slashook::{command, commands::Command};
 use sysinfo::{get_current_pid, ProcessExt, System, SystemExt};
 
@@ -10,24 +14,20 @@ pub fn get_command() -> Command {
         match get_current_pid() {
             Ok(pid) => {
                 let mut system = System::new();
-
                 system.refresh_process(pid);
 
                 let process = system.process(pid).context("Could not get process.")?;
 
                 res.send_message(
-                    vec![
-                        ["uptime".into(), format_timestamp!(process.start_time())],
-                        ["memory".into(), format!("{} MB", process.memory() / 1024 / 1024)],
-                        [
-                            "virtual memory".into(),
+                    Embed::new()
+                        .set_color(PRIMARY_COLOR)?
+                        .add_field("Uptime", format_timestamp!(process.start_time()), false)
+                        .add_field("Memory", format!("{} MB", process.memory() / 1024 / 1024), false)
+                        .add_field(
+                            "Virtual Memory",
                             format!("{} MB", process.virtual_memory() / 1024 / 1024),
-                        ],
-                    ]
-                    .iter()
-                    .map(|[k, v]| format!("{k}: {v}"))
-                    .collect::<Vec<String>>()
-                    .join("\n"),
+                            false,
+                        ),
                 )
                 .await?;
             },
