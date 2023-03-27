3482a43e6679bc275a4e15e3f70d7467ffb9346f
flazepe <flazepe@gmail.com>
2023-03-28 03:36:05 +0800

    refactor: send response before deleting reminder

--- src/commands/utils/remind/delete.rs
+++ src/commands/utils/remind/delete.rs
@@ -46,9 +46,8 @@ pub async fn run(input: CommandInput, res: CommandResponder) -> Result<()> {
 
     match entries.get(input.get_string_arg("entry")?.parse::<usize>()? - 1) {
         Some(entry) => {
-            reminders.delete_one(doc! { "_id": entry._id }, None).await?;
-
             res.send_message(format!("{SUCCESS_EMOJI} Gone.")).await?;
+            reminders.delete_one(doc! { "_id": entry._id }, None).await?;
         },
         None => {
             res.send_message(format!("{ERROR_EMOJI} Invalid entry. Make sure it's a valid number."))
