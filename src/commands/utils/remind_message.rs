5ea08639c5edbd74975205e903b97b8b59373cfe
flazepe <flazepe@gmail.com>
2023-03-30 02:57:02 +0800

    feat: make message reminder select menu ephemeral again

--- src/commands/utils/remind_message.rs
+++ src/commands/utils/remind_message.rs
@@ -14,21 +14,24 @@ pub fn get_command() -> Command {
 		command_type = ApplicationCommandType::MESSAGE,
 	)]
     async fn remind_message(input: CommandInput, res: CommandResponder) {
-        res.send_message(MessageResponse::from(
-            Components::new().add_select_menu(
-                add_reminder_select_options!(SelectMenu::new(SelectMenuType::STRING))
-                    .set_id(
-                        "remind",
-                        format!(
-                            "{}/{}/{}",
-                            input.guild_id.as_ref().unwrap_or(&"@me".into()),
-                            input.channel_id.as_ref().unwrap(),
-                            input.target_message.unwrap().id
-                        ),
-                    )
-                    .set_placeholder("Select time to remind about message"),
-            ),
-        ))
+        res.send_message(
+            MessageResponse::from(
+                Components::new().add_select_menu(
+                    add_reminder_select_options!(SelectMenu::new(SelectMenuType::STRING))
+                        .set_id(
+                            "remind",
+                            format!(
+                                "{}/{}/{}",
+                                input.guild_id.as_ref().unwrap_or(&"@me".into()),
+                                input.channel_id.as_ref().unwrap(),
+                                input.target_message.unwrap().id
+                            ),
+                        )
+                        .set_placeholder("Select time to remind about message"),
+                ),
+            )
+            .set_ephemeral(true),
+        )
         .await?;
     }
 
