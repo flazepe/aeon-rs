cda6eb3812a842f2bb3beac9fa5f7315cf51f16b
flazepe <flazepe@gmail.com>
2023-04-09 08:54:57 +0800

    feat: implement some helper fns to AniList

--- src/structs/api/anilist/mod.rs
+++ src/structs/api/anilist/mod.rs
@@ -2,10 +2,14 @@ mod anime;
 mod components;
 mod manga;
 
+use crate::macros::{format_timestamp, if_else};
+use crate::structs::api::anilist::components::AniListFuzzyDate;
 use anyhow::Result;
 use reqwest::Client;
 use serde::de::DeserializeOwned;
 use serde_json::{json, Value};
+use slashook::chrono::NaiveDateTime;
+use std::fmt::Debug;
 
 pub struct AniList {}
 
@@ -25,5 +29,52 @@ impl AniList {
             .await?
             .json::<U>()
             .await?)
+    }
+
+    fn prettify_enum_value<T: Debug>(value: T) -> String {
+        format!("{:?}", value)
+            .split("_")
+            .map(|word| {
+                if_else!(
+                    word == "TV",
+                    word.into(),
+                    format!(
+                        "{}{}",
+                        word.chars().next().unwrap(),
+                        word.chars().skip(1).collect::<String>().to_lowercase()
+                    )
+                )
+            })
+            .collect::<Vec<String>>()
+            .join(" ")
+    }
+
+    fn format_airing_date(start: AniListFuzzyDate, end: AniListFuzzyDate) -> String {
+        let mut dates = vec![];
+
+        for fuzzy_date in [start, end] {
+            if let Some(date) = if_else!(
+                fuzzy_date.day.is_none() || fuzzy_date.month.is_none() || fuzzy_date.year.is_none(),
+                None,
+                Some(format_timestamp!(
+                    NaiveDateTime::parse_from_str(
+                        &format!(
+                            "{}-{}-{} 00:00:00",
+                            fuzzy_date.year.unwrap(),
+                            fuzzy_date.month.unwrap(),
+                            fuzzy_date.day.unwrap()
+                        ),
+                        "%Y-%m-%d %H:%M:%S"
+                    )
+                    .unwrap()
+                    .timestamp(),
+                    "simple"
+                ))
+            ) {
+                dates.push(date);
+            }
+        }
+
+        if_else!(dates.is_empty(), "TBA".into(), dates.join(" - "))
     }
 }
