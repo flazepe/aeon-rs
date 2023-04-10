03eecb8642fa68c918409d01b08e05ab6dee5171
flazepe <flazepe@gmail.com>
2023-04-10 11:19:29 +0800

    feat: add tag and trait fields to vndb statics

--- src/statics/vndb.rs
+++ src/statics/vndb.rs
@@ -1,3 +1,7 @@
 pub static VISUAL_NOVEL_FIELDS: &str = "id,title,alttitle,titles{lang,title,latin,official,main},aliases,olang,devstatus,released,languages,platforms,image{id,url,dims,sexual,violence,votecount},length,length_minutes,length_votes,description,rating,popularity,votecount,tags{rating,spoiler,lie,id,name,aliases,description,category,searchable,applicable,vn_count}";
 
 pub static CHARACTER_FIELDS: &str = "id,name,original,aliases,description,image{id,url,dims,sexual,violence,votecount},blood_type,height,weight,bust,waist,hips,cup,age,birthday,sex,vns{id,title,role},traits{id,name,aliases,description,searchable,applicable,group_id,group_name,char_count,spoiler,lie}";
+
+pub static TAG_FIELDS: &str = "id,name,aliases,description,category,searchable,applicable,vn_count";
+
+pub static TRAIT_FIELDS: &str = "id,name,aliases,description,searchable,applicable,group_id,group_name,char_count";
