64e835ec2f2d48a2bd044c533e606355bef00a3d
flazepe <flazepe@gmail.com>
2023-04-13 20:23:24 +0800

    feat: add more emojis

--- src/statics/emojis.rs
+++ src/statics/emojis.rs
@@ -1,2 +1,7 @@
 pub static ERROR_EMOJI: &str = "<:error:841838403821633569>";
 pub static SUCCESS_EMOJI: &str = "<:success:841838404479352872>";
+
+pub static EXPLICIT_EMOJI: &str = "<:explicit:847028265604349982>";
+pub static FIRE_EMOJI: &str = "<:fire:893695259350007808>";
+pub static COPYRIGHT_EMOJI: &str = "<:copyright:862772853425438770>";
+pub static PHONOGRAM_EMOJI: &str = "<:phonogram:862772853564506162>";
