9cc990501c6768391438739036632c49de508309
flazepe <flazepe@gmail.com>
2023-04-19 09:42:02 +0800

    refactor: Ok()

--- src/commands/utility/tag/view.rs
+++ src/commands/utility/tag/view.rs
@@ -6,35 +6,35 @@ use slashook::{
 };
 
 pub async fn run(input: CommandInput, res: CommandResponder) -> Result<()> {
-    match Tags::new()
-        .get(input.get_string_arg("tag")?, input.guild_id.as_ref().unwrap())
-        .await
-    {
-        Ok(tag) => {
-            if let Some(channel) = Channel::fetch(&input.rest, input.channel_id.as_ref().unwrap())
-                .await
-                .ok()
-            {
-                if !channel.nsfw.unwrap_or(false) && tag.nsfw {
-                    res.send_message(format!("{ERROR_EMOJI} Tag is for NSFW channels only."))
-                        .await?;
+    Ok(
+        match Tags::new()
+            .get(input.get_string_arg("tag")?, input.guild_id.as_ref().unwrap())
+            .await
+        {
+            Ok(tag) => {
+                if let Some(channel) = Channel::fetch(&input.rest, input.channel_id.as_ref().unwrap())
+                    .await
+                    .ok()
+                {
+                    if !channel.nsfw.unwrap_or(false) && tag.nsfw {
+                        res.send_message(format!("{ERROR_EMOJI} Tag is for NSFW channels only."))
+                            .await?;
 
-                    return Ok(());
+                        return Ok(());
+                    }
                 }
-            }
 
-            res.send_message(if_else!(
-                input.get_bool_arg("raw").unwrap_or(false),
-                format!("```\n{}```", tag.content.replace("`", "`\u{200b}")),
-                tag.content,
-            ))
-            .await?
+                res.send_message(if_else!(
+                    input.get_bool_arg("raw").unwrap_or(false),
+                    format!("```\n{}```", tag.content.replace("`", "`\u{200b}")),
+                    tag.content,
+                ))
+                .await?;
+            },
+            Err(error) => {
+                res.send_message(MessageResponse::from(format!("{ERROR_EMOJI} {error}")).set_ephemeral(true))
+                    .await?;
+            },
         },
-        Err(error) => {
-            res.send_message(MessageResponse::from(format!("{ERROR_EMOJI} {error}")).set_ephemeral(true))
-                .await?
-        },
-    };
-
-    Ok(())
+    )
 }
