f9e360c04a10bb15740c581564ca2e2b53e0dd23
flazepe <flazepe@gmail.com>
2023-05-16 02:53:04 +0800

    feat: add more emojis

--- src/statics/emojis.rs
+++ src/statics/emojis.rs
@@ -1,7 +1,20 @@
 pub static ERROR_EMOJI: &str = "<:error:841838403821633569>";
 pub static SUCCESS_EMOJI: &str = "<:success:841838404479352872>";
 
+pub static ONLINE_EMOJI: &str = "<:online:842590611386793995> ";
+pub static OFFLINE_EMOJI: &str = "<:offline:708124841890545685> ";
+
 pub static EXPLICIT_EMOJI: &str = "<:explicit:847028265604349982>";
 pub static FIRE_EMOJI: &str = "<:fire:893695259350007808>";
 pub static COPYRIGHT_EMOJI: &str = "<:copyright:862772853425438770>";
 pub static PHONOGRAM_EMOJI: &str = "<:phonogram:862772853564506162>";
+
+pub static OSU_SUPPORTER_1_EMOJI: &str = "<:osu_supporter_1_1:877932101209567232><:osu_supporter_1_2:877932101352173579>";
+pub static OSU_SUPPORTER_2_EMOJI: &str = "<:osu_supporter_2_1:877932102165876738><:osu_supporter_2_2:877932102723727371>";
+pub static OSU_SUPPORTER_3_EMOJI: &str = "<:osu_supporter_3_1:877932103432552448><:osu_supporter_3_2:877932104002994186>";
+
+pub static OSU_X_EMOJI: &str = "<:osu_x_1:877915085857755217><:osu_x_2:877915085631258664>";
+pub static OSU_XH_EMOJI: &str = "<:osu_xh_1:877915085388021801><:osu_xh_2:877915085820022835>";
+pub static OSU_S_EMOJI: &str = "<:osu_s_1:877915084477857872><:osu_s_2:877915084456869888>";
+pub static OSU_SH_EMOJI: &str = "<:osu_sh_1:877915084276531221><:osu_sh_2:877915084540764170>";
+pub static OSU_A_EMOJI: &str = "<:osu_a_1:877915083613798400><:osu_a_2:877915083760599080>";
