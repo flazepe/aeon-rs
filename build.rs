3a7ad910c3abf4452899e202ccc577ec8001993d
flazepe <flazepe@gmail.com>
2023-05-15 17:10:44 +0800

    chore: remove unneeded dependency

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ fn main() -> () {
     tonic_build::configure()
         .build_client(true)
         .build_server(false)
-        .out_dir("src/structs/api/google/proto")
+        .out_dir("src/structs/api/google/protos")
         .compile(&["google/assistant/embedded/v1alpha2/embedded_assistant.proto"], &["proto"])
         .unwrap();
     */
