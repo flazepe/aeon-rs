1b644b50c631645fbba5a4f74b11d9e3989ace94
flazepe <flazepe@gmail.com>
2023-05-21 15:43:05 +0800

    refactor: use `ToString` instead

--- src/traits.rs
+++ src/traits.rs
@@ -1,14 +1,13 @@
 use slashook::structs::users::User;
-use std::fmt::Display;
 use twilight_model::user::User as TwilightUser;
 
 pub trait AvatarURL {
-    fn avatar_url<T: Display, U: Display>(&self, format: T, size: U) -> Option<String>;
-    fn display_avatar_url<T: Display, U: Display>(&self, format: T, size: U) -> String;
+    fn avatar_url<T: ToString, U: ToString>(&self, format: T, size: U) -> Option<String>;
+    fn display_avatar_url<T: ToString, U: ToString>(&self, format: T, size: U) -> String;
 }
 
 impl AvatarURL for User {
-    fn avatar_url<T: Display, U: Display>(&self, format: T, size: U) -> Option<String> {
+    fn avatar_url<T: ToString, U: ToString>(&self, format: T, size: U) -> Option<String> {
         let mut format = format.to_string();
 
         if let Some(avatar) = self.avatar.as_ref() {
@@ -20,7 +19,7 @@ impl AvatarURL for User {
         self.avatar_url(format, size)
     }
 
-    fn display_avatar_url<T: Display, U: Display>(&self, format: T, size: U) -> String {
+    fn display_avatar_url<T: ToString, U: ToString>(&self, format: T, size: U) -> String {
         match AvatarURL::avatar_url(self, format, size) {
             Some(avatar_url) => avatar_url,
             None => format!("https://cdn.discordapp.com/embed/avatars/{}.png", (self.id.parse::<u64>().unwrap() >> 22) % 5),
@@ -29,7 +28,7 @@ impl AvatarURL for User {
 }
 
 impl AvatarURL for TwilightUser {
-    fn avatar_url<T: Display, U: Display>(&self, format: T, size: U) -> Option<String> {
+    fn avatar_url<T: ToString, U: ToString>(&self, format: T, size: U) -> Option<String> {
         let mut format = format.to_string();
 
         if let Some(avatar) = self.avatar.as_ref() {
@@ -38,10 +37,10 @@ impl AvatarURL for TwilightUser {
             }
         }
 
-        self.avatar.as_ref().map(|a| format!("https://cdn.discordapp.com/avatars/{}/{a}.{format}?size={size}", self.id))
+        self.avatar.as_ref().map(|a| format!("https://cdn.discordapp.com/avatars/{}/{a}.{format}?size={}", self.id, size.to_string()))
     }
 
-    fn display_avatar_url<T: Display, U: Display>(&self, format: T, size: U) -> String {
+    fn display_avatar_url<T: ToString, U: ToString>(&self, format: T, size: U) -> String {
         match AvatarURL::avatar_url(self, format, size) {
             Some(avatar_url) => avatar_url,
             None => format!("https://cdn.discordapp.com/embed/avatars/{}.png", (self.id.to_string().parse::<u64>().unwrap() >> 22) % 5),
