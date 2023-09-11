18570794379b7b834591135ef60dc96ae59c6f45
flazepe <flazepe@gmail.com>
2023-09-12 00:14:02 +0800

    feat: spotify card subcommand

---
+++ src/commands/search/spotify/card.rs
@@ -0,0 +1,74 @@
+use crate::{
+    statics::CACHE,
+    structs::{command_context::CommandContext, gateway::cache::SongActivityStyle},
+    traits::AvatarUrl,
+};
+use anyhow::Result;
+use serde::Serialize;
+use serde_json::to_string;
+use slashook::structs::utils::File;
+use tokio::process::Command;
+
+pub async fn run(ctx: CommandContext) -> Result<()> {
+    let mut user = ctx.get_user_arg("member").unwrap_or(&ctx.input.user);
+
+    // Set to author if there's no resolved member
+    if ctx.input.resolved.as_ref().and_then(|resolved| resolved.members.as_ref().and_then(|members| members.values().next())).is_none() {
+        user = &ctx.input.user;
+    }
+
+    let activity;
+
+    {
+        let activities = CACHE.spotify.read().unwrap();
+        activity = activities.get(&user.id).cloned();
+    }
+
+    match activity {
+        Some(activity) => {
+            ctx.res.defer(false).await?;
+
+            let mut activity = activity.clone();
+
+            // Set to proper style
+            if let Ok(style) = ctx.get_string_arg("style").as_deref() {
+                activity.style = match style {
+                    "classic" => SongActivityStyle::Classic,
+                    "nori" => SongActivityStyle::Nori,
+                    "rovi" => SongActivityStyle::Rovi,
+                    "vxc" => SongActivityStyle::VXC,
+
+                    // Default card style is nori's
+                    _ => SongActivityStyle::Nori,
+                };
+            }
+
+            // Set to user display avatar if track is local
+            if activity.album_cover.is_empty() {
+                activity.album_cover = user.display_avatar_url("png", 4096);
+            }
+
+            ctx.respond(
+                File::new(
+                    "image.png",
+                    Command::new("node").args(["../aeon-song-card-generator", &to_string(&activity)?]).output().await?.stdout,
+                ),
+                false,
+            )
+            .await
+        },
+        None => ctx.respond_error(format!("No Spotify activity found for <@{}>.", user.id), true).await,
+    }
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SongCardInput {
+    pub service: String,
+    pub style: String,
+    pub title: String,
+    pub artist: String,
+    pub album: String,
+    pub album_cover: String,
+    pub timestamps: (u64, u64),
+}
--- src/commands/search/spotify/mod.rs
+++ src/commands/search/spotify/mod.rs
@@ -1,4 +1,5 @@
 mod album;
+mod card;
 mod song;
 
 use crate::structs::command::Command;
@@ -6,10 +7,11 @@ use once_cell::sync::Lazy;
 use slashook::{
     command,
     commands::{Command as SlashookCommand, CommandInput, CommandResponder},
-    structs::interactions::InteractionOptionType,
+    structs::interactions::{ApplicationCommandOptionChoice, InteractionOptionType},
 };
 
-static COMMAND: Lazy<Command> = Lazy::new(|| Command::new().subcommand("album", album::run).subcommand("song", song::run));
+static COMMAND: Lazy<Command> =
+    Lazy::new(|| Command::new().subcommand("album", album::run).subcommand("card", card::run).subcommand("song", song::run));
 
 pub fn get_command() -> SlashookCommand {
     #[command(
@@ -47,6 +49,28 @@ pub fn get_command() -> SlashookCommand {
                         name = "search",
                         description = "Whether to search",
                         option_type = InteractionOptionType::BOOLEAN,
+                    },
+                ],
+            },
+            {
+                name = "card",
+                description = "Generates a card from a member's Spotify activity.",
+                options = [
+                    {
+                        name = "member",
+                        description = "The member to show the card for",
+                        option_type = InteractionOptionType::USER,
+                    },
+                    {
+                        name = "style",
+                        description = "The card style",
+                        option_type = InteractionOptionType::STRING,
+                        choices = [
+                            ApplicationCommandOptionChoice::new("Classic", "classic"),
+                            ApplicationCommandOptionChoice::new("Nori", "nori"),
+                            ApplicationCommandOptionChoice::new("Rovi", "rovi"),
+                            ApplicationCommandOptionChoice::new("VXC", "vxc"),
+                        ],
                     },
                 ],
             },
