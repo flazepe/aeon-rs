570358f1c6a25531a5a64988bc35d534c2484bd9
flazepe <flazepe@gmail.com>
2024-02-23 04:04:15 +0800

    feat: check reaction snipe channel id if given a url

--- src/commands/utility/snipe/reaction.rs
+++ src/commands/utility/snipe/reaction.rs
@@ -4,13 +4,10 @@ use anyhow::Result;
 pub async fn run(ctx: CommandContext) -> Result<()> {
     let message = ctx.get_string_arg("message")?;
 
-    match ReactionSnipes::new(
-        ctx.input.guild_id.as_ref().unwrap(),
-        ctx.input.channel_id.as_ref().unwrap(),
-        message.split('/').last().unwrap(),
-    )
-    .to_response()
-    {
+    let mut split = message.split('/').rev();
+    let (message_id, channel_id) = (split.next().unwrap(), split.next().unwrap_or(ctx.input.channel_id.as_ref().unwrap()));
+
+    match ReactionSnipes::new(ctx.input.guild_id.as_ref().unwrap(), channel_id, message_id).to_response() {
         Ok(response) => ctx.respond(response, false).await,
         Err(error) => ctx.respond_error(error, true).await,
     }
