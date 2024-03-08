d0c11fffc44b951945e9241f78774c6f0f80bb26
flazepe <flazepe@gmail.com>
2024-03-09 02:25:54 +0800

    feat: show stickers in stringified message

--- src/structs/stringified_message.rs
+++ src/structs/stringified_message.rs
@@ -1,7 +1,14 @@
 use crate::functions::escape_markdown;
-use slashook::structs::channels::Message;
+use slashook::structs::{channels::Message, stickers::StickerFormatType};
 use std::fmt::{Display, Formatter, Result as FmtResult};
-use twilight_model::channel::Message as TwilightMessage;
+use twilight_model::channel::{message::sticker::StickerFormatType as TwilightStickerFormatType, Message as TwilightMessage};
+
+pub struct StringifiedMessage {
+    pub content: String,
+    pub embeds: Vec<SimpleEmbed>,
+    pub attachments: Vec<(String, String)>,
+    pub stickers: Vec<SimpleSticker>,
+}
 
 pub struct SimpleEmbed {
     title: Option<String>,
@@ -12,10 +19,10 @@ pub struct SimpleEmbed {
     fields: Vec<(String, String)>,
 }
 
-pub struct StringifiedMessage {
-    pub content: String,
-    pub embeds: Vec<SimpleEmbed>,
-    pub attachments: Vec<(String, String)>,
+pub struct SimpleSticker {
+    id: String,
+    name: String,
+    format: String,
 }
 
 impl From<Message> for StringifiedMessage {
@@ -40,6 +47,21 @@ impl From<Message> for StringifiedMessage {
                 })
                 .collect::<Vec<SimpleEmbed>>(),
             attachments: message.attachments.into_iter().map(|attachment| (attachment.filename, attachment.url)).collect(),
+            stickers: message
+                .sticker_items
+                .unwrap_or_default()
+                .into_iter()
+                .map(|sticker| SimpleSticker {
+                    id: sticker.id,
+                    name: sticker.name,
+                    format: match sticker.format_type {
+                        StickerFormatType::PNG => "png".into(),
+                        StickerFormatType::APNG => "apng".into(),
+                        StickerFormatType::LOTTIE => "lottie".into(),
+                        StickerFormatType::UNKNOWN => "png".into(),
+                    },
+                })
+                .collect::<Vec<SimpleSticker>>(),
         }
     }
 }
@@ -61,6 +83,22 @@ impl From<TwilightMessage> for StringifiedMessage {
                 })
                 .collect::<Vec<SimpleEmbed>>(),
             attachments: message.attachments.into_iter().map(|attachment| (attachment.filename, attachment.url)).collect(),
+            stickers: message
+                .sticker_items
+                .into_iter()
+                .map(|sticker| SimpleSticker {
+                    id: sticker.id.to_string(),
+                    name: sticker.name,
+                    format: match sticker.format_type {
+                        TwilightStickerFormatType::Png => "png".into(),
+                        TwilightStickerFormatType::Apng => "apng".into(),
+                        TwilightStickerFormatType::Lottie => "lottie".into(),
+                        TwilightStickerFormatType::Gif => "gif".into(),
+                        TwilightStickerFormatType::Unknown(_) => "png".into(),
+                        _ => "png".into(),
+                    },
+                })
+                .collect::<Vec<SimpleSticker>>(),
         }
     }
 }
@@ -70,6 +108,19 @@ impl Display for StringifiedMessage {
         let mut text = self.attachments.iter().map(|(name, url)| format!("[{name}]({url})")).collect::<Vec<String>>().join("\n");
 
         text += &format!("\n\n{}", self.content);
+
+        if !self.stickers.is_empty() {
+            text += &format!(
+                "\n\n{}",
+                self.stickers
+                    .iter()
+                    .map(|sticker| {
+                        format!("[{}](https://cdn.discordapp.com/stickers/{}.{}?size=4096)", sticker.name, sticker.id, sticker.format)
+                    })
+                    .collect::<Vec<String>>()
+                    .join("\n"),
+            );
+        }
 
         for embed in &self.embeds {
             if let Some(author_name) = embed.author_name.as_ref() {
