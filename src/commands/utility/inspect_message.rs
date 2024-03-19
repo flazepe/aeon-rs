d58f3f3623c8506906d6a51a4fa7e8beee64127f
flazepe <flazepe@gmail.com>
2024-03-20 00:56:59 +0800

    feat: add json file to inspect message cmd

--- src/commands/utility/inspect_message.rs
+++ src/commands/utility/inspect_message.rs
@@ -1,18 +1,36 @@
 use crate::structs::{command::Command, command_context::CommandContext};
 use once_cell::sync::Lazy;
+use serde_json::{to_string_pretty, Value};
 use slashook::{
     command,
     commands::{Command as SlashookCommand, CommandInput, CommandResponder, MessageResponse},
     structs::{
         interactions::{ApplicationCommandType, IntegrationType, InteractionContextType},
         utils::File,
+        Permissions,
     },
 };
 
 static COMMAND: Lazy<Command> = Lazy::new(|| {
     Command::new().main(|ctx: CommandContext| async move {
-        ctx.respond(MessageResponse::from(File::new("message.rs", format!("{:#?}", ctx.input.target_message.as_ref().unwrap()))), true)
-            .await
+        let mut response = MessageResponse::from(File::new("message.rs", format!("{:#?}", ctx.input.target_message.as_ref().unwrap())));
+
+        if ctx.input.app_permissions.unwrap().contains(Permissions::VIEW_CHANNEL | Permissions::READ_MESSAGE_HISTORY) {
+            if let Ok(value) = ctx
+                .input
+                .rest
+                .get::<Value>(format!(
+                    "channels/{}/messages/{}",
+                    ctx.input.channel_id.as_ref().unwrap(),
+                    ctx.input.target_message.as_ref().unwrap().id,
+                ))
+                .await
+            {
+                response = response.add_file(File::new("message.json", to_string_pretty(&value)?))
+            }
+        }
+
+        ctx.respond(response, true).await
     })
 });
 
