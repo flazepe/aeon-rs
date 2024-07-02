afb68715562163590acf034fa1b88c2d5c464b9d
flazepe <flazepe@gmail.com>
2024-07-02 18:09:08 +0800

    feat: improved distrowatch embed

--- src/commands/search/distrowatch.rs
+++ src/commands/search/distrowatch.rs
@@ -1,4 +1,4 @@
-use crate::structs::{command::Command, command_context::CommandContext, scraping::distrowatch::Distro};
+use crate::structs::{command::Command, command_context::CommandContext, scraping::distrowatch::Distribution};
 use once_cell::sync::Lazy;
 use slashook::{
     command,
@@ -8,8 +8,8 @@ use slashook::{
 
 static COMMAND: Lazy<Command> = Lazy::new(|| {
     Command::new().main(|ctx: CommandContext| async move {
-        match Distro::get(ctx.get_string_arg("distro")?).await {
-            Ok(distro) => ctx.respond(distro.format(), false).await,
+        match Distribution::get(ctx.get_string_arg("distribution")?).await {
+            Ok(distribution) => ctx.respond(distribution.format(), false).await,
             Err(error) => ctx.respond_error(error, true).await,
         }
     })
@@ -23,7 +23,7 @@ pub fn get_command() -> SlashookCommand {
         contexts = [InteractionContextType::GUILD, InteractionContextType::BOT_DM, InteractionContextType::PRIVATE_CHANNEL],
         options = [
             {
-                name = "distro",
+                name = "distribution",
                 description = "The distribution",
                 option_type = InteractionOptionType::STRING,
                 required = true,
--- src/structs/scraping/distrowatch.rs
+++ src/structs/scraping/distrowatch.rs
@@ -4,9 +4,12 @@ use nipper::Document;
 use slashook::structs::embeds::Embed;
 use std::fmt::Display;
 
-pub struct Distro {
+const BASE_DOMAIN: &str = "https://distrowatch.com";
+
+pub struct Distribution {
     pub name: String,
-    pub distro_type: String,
+    pub url: String,
+    pub distribution_type: String,
     pub architecture: String,
     pub based_on: String,
     pub origin: String,
@@ -16,19 +19,16 @@ pub struct Distro {
     pub popularity: String,
 }
 
-impl Distro {
+impl Distribution {
     pub async fn get<T: Display>(name: T) -> Result<Self> {
-        let document = Document::from(
-            &REQWEST
-                .get("https://distrowatch.com/table.php")
-                .query(&[("distribution", name.to_string())])
-                .header("user-agent", "yes")
-                .send()
-                .await?
-                .text()
-                .await?,
-        );
-
+        let res = REQWEST
+            .get(format!("{BASE_DOMAIN}/table.php"))
+            .query(&[("distribution", name.to_string())])
+            .header("user-agent", "yes")
+            .send()
+            .await?;
+        let url = res.url().to_string();
+        let document = Document::from(&res.text().await?);
         let name = document.select("td.TablesTitle h1").text();
 
         if name.is_empty() {
@@ -42,12 +42,14 @@ impl Distro {
                 .split(':')
                 .last()
                 .context(format!("Could not get table nth child value for {n}."))?
+                .trim()
                 .to_string())
         };
 
         Ok(Self {
             name: name.to_string(),
-            distro_type: get_table_nth_child(1)?,
+            url,
+            distribution_type: get_table_nth_child(1)?,
             architecture: get_table_nth_child(4)?,
             based_on: get_table_nth_child(2)?,
             origin: get_table_nth_child(3)?,
@@ -59,17 +61,26 @@ impl Distro {
     }
 
     pub fn format(&self) -> Embed {
+        fn to_urls<T: Display>(names: T, url_type: &str) -> String {
+            names
+                .to_string()
+                .split(", ")
+                .map(|name| format!("[{name}]({BASE_DOMAIN}/search.php?{url_type}={})", name.replace(' ', "+")))
+                .collect::<Vec<String>>()
+                .join(", ")
+        }
+
         Embed::new()
             .set_color(PRIMARY_COLOR)
             .unwrap_or_default()
-            .add_field("Name", &self.name, true)
-            .add_field("Type", &self.distro_type, true)
-            .add_field("Architecture", &self.architecture, true)
-            .add_field("Based on", &self.based_on, true)
-            .add_field("Origin", &self.origin, true)
+            .add_field("Name", format!("[{}]({})", self.name, self.url), true)
+            .add_field("Type", to_urls(&self.distribution_type, "ostype"), true)
+            .add_field("Architecture", to_urls(&self.architecture, "architecture"), true)
+            .add_field("Based on", to_urls(&self.based_on, "basedon"), true)
+            .add_field("Origin", to_urls(&self.origin, "origin"), true)
             .add_field("Status", &self.status, true)
-            .add_field("Category", &self.category, true)
-            .add_field("Desktop", &self.desktop, true)
-            .add_field("Popularity", &self.popularity, true)
+            .add_field("Category", to_urls(&self.category, "category"), true)
+            .add_field("Desktop", to_urls(&self.desktop, "desktop"), true)
+            .add_field("Popularity", format!("[{}]({BASE_DOMAIN}/dwres.php?resource=popularity)", &self.popularity), true)
     }
 }
