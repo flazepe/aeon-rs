6acae4196f9ebc90afdbedf01e2d540777167550
flazepe <flazepe@gmail.com>
2024-07-27 03:30:15 +0800

    feat: select menu for lyricfind command

--- src/commands/search/lyricfind.rs
+++ src/commands/search/lyricfind.rs
@@ -1,16 +1,20 @@
 use crate::{
     statics::CACHE,
-    structs::{api::lyricfind::LyricFind, command::Command, command_context::CommandContext},
+    structs::{api::lyricfind::LyricFind, command::Command, command_context::CommandContext, select_menu::SelectMenu},
 };
 use once_cell::sync::Lazy;
 use slashook::{
     command,
-    commands::{Command as SlashookCommand, CommandInput, CommandResponder},
+    commands::{Command as SlashookCommand, CommandInput, CommandResponder, MessageResponse},
     structs::interactions::{IntegrationType, InteractionContextType, InteractionOptionType},
 };
 
 static COMMAND: Lazy<Command> = Lazy::new(|| {
     Command::new().main(|ctx: CommandContext| async move {
+        if ctx.input.is_string_select() {
+            return ctx.respond(LyricFind::search(&ctx.input.values.as_ref().unwrap()[0]).await?[0].format(), false).await;
+        }
+
         let mut query = ctx.get_string_arg("song");
 
         if query.is_err() {
@@ -21,10 +25,18 @@ static COMMAND: Lazy<Command> = Lazy::new(|| {
 
         let Ok(query) = query else { return ctx.respond_error("Please provide a song.", true).await };
 
-        match LyricFind::search(query).await {
-            Ok(mut lyricfind) => ctx.respond(lyricfind.remove(0).format(), false).await,
-            Err(error) => ctx.respond_error(error, true).await,
+        let tracks = match LyricFind::search(&query).await {
+            Ok(tracks) => tracks,
+            Err(error) => return ctx.respond_error(error, true).await,
+        };
+
+        let mut select_menu = SelectMenu::new("lyricfind", "search", "View other results…", None::<String>);
+
+        for track in &tracks {
+            select_menu = select_menu.add_option(&track.title, format!("{query} {}", &track.artist.name), Some(track.artist.name.clone()));
         }
+
+        ctx.respond(MessageResponse::from(select_menu).add_embed(tracks[0].format()), false).await
     })
 });
 
