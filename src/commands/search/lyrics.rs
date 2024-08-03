3aa8f2c5900b60ec45e4f7dc327308942af1e3cb
flazepe <flazepe@gmail.com>
2024-08-03 17:31:47 +0800

    fix: defer lyrics command

--- src/commands/search/lyrics.rs
+++ src/commands/search/lyrics.rs
@@ -19,6 +19,8 @@ static COMMAND: Lazy<Command> = Lazy::new(|| {
             return ctx.respond_error("Please provide a song.", true).await;
         };
 
+        ctx.res.defer(false).await?;
+
         let Ok(mut track) = Spotify::search_track(query).await else { return ctx.respond_error("Song not found.", true).await };
 
         match Spotify::get_lyrics(track.remove(0)).await {
