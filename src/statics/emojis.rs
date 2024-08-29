3f9d3d6fbac2876d83d0a8a37304467cdbfcacbb
flazepe <flazepe@gmail.com>
2024-08-29 15:18:30 +0800

    feat: use new emoji IDs

--- src/statics/emojis.rs
+++ src/statics/emojis.rs
@@ -1,20 +1,20 @@
-pub static ERROR_EMOJI: &str = "<:error:841838403821633569>";
-pub static SUCCESS_EMOJI: &str = "<:success:841838404479352872>";
+pub static ERROR_EMOJI: &str = "<:error:1278613720330666004>";
+pub static SUCCESS_EMOJI: &str = "<:success:1278613705176776746>";
 
-pub static ONLINE_EMOJI: &str = "<:online:842590611386793995> ";
-pub static OFFLINE_EMOJI: &str = "<:offline:708124841890545685> ";
+pub static ONLINE_EMOJI: &str = "<:online:1278613790967201822> ";
+pub static OFFLINE_EMOJI: &str = "<:offline:1278613782439919627> ";
 
-pub static EXPLICIT_EMOJI: &str = "<:explicit:847028265604349982>";
-pub static FIRE_EMOJI: &str = "<:fire:893695259350007808>";
-pub static COPYRIGHT_EMOJI: &str = "<:copyright:862772853425438770>";
-pub static PHONOGRAM_EMOJI: &str = "<:phonogram:862772853564506162>";
+pub static EXPLICIT_EMOJI: &str = "<:explicit:1278613753486643266>";
+pub static FIRE_EMOJI: &str = "<:fire:1278613771371155456>";
+pub static COPYRIGHT_EMOJI: &str = "<:copyright:1278613731781247070>";
+pub static PHONOGRAM_EMOJI: &str = "<:phonogram:1278613743525298218>";
 
-pub static OSU_SUPPORTER_1_EMOJI: &str = "<:osu_supporter_1_1:877932101209567232><:osu_supporter_1_2:877932101352173579>";
-pub static OSU_SUPPORTER_2_EMOJI: &str = "<:osu_supporter_2_1:877932102165876738><:osu_supporter_2_2:877932102723727371>";
-pub static OSU_SUPPORTER_3_EMOJI: &str = "<:osu_supporter_3_1:877932103432552448><:osu_supporter_3_2:877932104002994186>";
+pub static OSU_SUPPORTER_1_EMOJI: &str = "<:osu_supporter_1_1:1278613947930382336><:osu_supporter_1_2:1278613958739103764>";
+pub static OSU_SUPPORTER_2_EMOJI: &str = "<:osu_supporter_2_1:1278613969589895189><:osu_supporter_2_2:1278613979689910323>";
+pub static OSU_SUPPORTER_3_EMOJI: &str = "<:osu_supporter_3_1:1278613989328293899><:osu_supporter_3_2:1278614000342536202>";
 
-pub static OSU_X_EMOJI: &str = "<:osu_x_1:877915085857755217><:osu_x_2:877915085631258664>";
-pub static OSU_XH_EMOJI: &str = "<:osu_xh_1:877915085388021801><:osu_xh_2:877915085820022835>";
-pub static OSU_S_EMOJI: &str = "<:osu_s_1:877915084477857872><:osu_s_2:877915084456869888>";
-pub static OSU_SH_EMOJI: &str = "<:osu_sh_1:877915084276531221><:osu_sh_2:877915084540764170>";
-pub static OSU_A_EMOJI: &str = "<:osu_a_1:877915083613798400><:osu_a_2:877915083760599080>";
+pub static OSU_X_EMOJI: &str = "<:osu_x_1:1278613878510456864><:osu_x_2:1278613889587744780>";
+pub static OSU_XH_EMOJI: &str = "<:osu_xh_1:1278613900463439892><:osu_xh_2:1278613911200989204>";
+pub static OSU_S_EMOJI: &str = "<:osu_s_1:1278613836319953048><:osu_s_2:1278613845358936106>";
+pub static OSU_SH_EMOJI: &str = "<:osu_sh_1:1278613854795862048><:osu_sh_2:1278613867093688391>";
+pub static OSU_A_EMOJI: &str = "<:osu_a_1:1278613816246145029><:osu_a_2:1278613826052296748>";
