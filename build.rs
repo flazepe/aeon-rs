bb96e8bd5234250bce8ab230b200d765d3edee19
flazepe <flazepe@gmail.com>
2025-03-13 18:16:30 +0800

    feat: update google assistant protos

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ fn main() {
         .build_client(true)
         .build_server(false)
         .out_dir("src/structs/api/google/protos")
-        .compile(&["google/assistant/embedded/v1alpha2/embedded_assistant.proto"], &["proto"])
+        .compile_protos(&["google/assistant/embedded/v1alpha2/embedded_assistant.proto"], &["proto"])
         .unwrap();
     */
 }
