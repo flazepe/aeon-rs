0ee7547b6f0b13b9437371560bcdcfdbc018a9ea
flazepe <flazepe@gmail.com>
2025-03-24 07:07:07 +0800

    feat: show diff in message_update embeds

--- src/structs/gateway/events/logs/message_update.rs
+++ src/structs/gateway/events/logs/message_update.rs
@@ -4,6 +4,7 @@ use crate::{
     traits::UserExt,
 };
 use anyhow::Result;
+use similar::{ChangeTag, TextDiff};
 use slashook::structs::embeds::Embed;
 use twilight_model::gateway::payload::incoming::MessageUpdate;
 
@@ -19,6 +20,25 @@ pub async fn handle(event: &MessageUpdate) -> Result<()> {
         return Ok(());
     }
 
+    let old_content = SimpleMessage::from(old_message).to_string();
+    let new_content = SimpleMessage::from(event.0.clone()).to_string();
+    let diff = TextDiff::from_words(&old_content, &new_content)
+        .iter_all_changes()
+        .map(|change| {
+            let string = change.as_str().unwrap_or_default().replace("~", "\\~").replace("*", "\\*");
+
+            if string.trim().is_empty() {
+                return string;
+            }
+
+            match change.tag() {
+                ChangeTag::Equal => string,
+                ChangeTag::Delete => format!("~~{string}~~"),
+                ChangeTag::Insert => format!("**{string}**"),
+            }
+        })
+        .collect::<String>();
+
     let embed = Embed::new()
         .set_color(NOTICE_COLOR)
         .unwrap_or_default()
@@ -30,8 +50,8 @@ pub async fn handle(event: &MessageUpdate) -> Result<()> {
             event.id,
         ))
         .add_field("Channel", format!("<#{channel_id}> ({channel_id})", channel_id = event.channel_id), false)
-        .add_field("Old Content", SimpleMessage::from(old_message).to_string().chars().take(1024).collect::<String>(), false)
-        .add_field("New Content", SimpleMessage::from(event.0.clone()).to_string().chars().take(1024).collect::<String>(), false)
+        .add_field("Before", old_content.chars().take(1024).collect::<String>(), false)
+        .add_field("After", diff.chars().take(1024).collect::<String>(), false)
         .set_footer(event.author.label(), Some(event.author.display_avatar_url("gif", 4096)));
 
     Guilds::send_log(guild_id, embed).await
