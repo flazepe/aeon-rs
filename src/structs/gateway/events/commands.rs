ffadf6fdb0c9564df05b6894898d9e0c4699d6f7
flazepe <flazepe@gmail.com>
2025-03-24 12:46:10 +0800

    feat: add `aeon` to default prefixes

--- src/structs/gateway/events/commands.rs
+++ src/structs/gateway/events/commands.rs
@@ -13,17 +13,18 @@ impl EventHandler {
     pub async fn handle_commands(message: &Message, sender: &MessageSender) -> Result<()> {
         let Some(guild_id) = &message.guild_id else { return Ok(()) };
         let guild = Guilds::get(guild_id).await?;
-        let mention_prefix = format!("<@{}>", CONFIG.bot.client_id);
-        let prefix = match guild.prefixes.iter().find(|prefix| message.content.to_lowercase().starts_with(*prefix)) {
-            Some(prefix) => prefix,
-            None => {
-                if message.content.starts_with(&mention_prefix) {
-                    &mention_prefix
-                } else {
-                    return Ok(());
-                }
-            },
+
+        let lowercased_content = message.content.to_lowercase();
+        let default_prefixes = [format!("<@{}>", CONFIG.bot.client_id), "aeon".into()];
+        let Some(prefix) = guild
+            .prefixes
+            .iter()
+            .find(|prefix| lowercased_content.starts_with(*prefix))
+            .or(default_prefixes.iter().find(|prefix| lowercased_content.starts_with(*prefix)))
+        else {
+            return Ok(());
         };
+
         let prefixless = message.content.chars().skip(prefix.len()).collect::<String>().trim().to_string();
         let (command, args) = prefixless.split_once(' ').unwrap_or((&prefixless, ""));
 
