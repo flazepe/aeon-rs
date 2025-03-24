16360bca9cd0e62d2d3f1d6895796df6d7eadc7f
flazepe <flazepe@gmail.com>
2025-03-24 10:27:57 +0800

    feat: prefix server config

--- src/commands/utility/server_config/mod.rs
+++ src/commands/utility/server_config/mod.rs
@@ -1,5 +1,6 @@
 mod fix_embeds;
 mod logs;
+mod prefix;
 mod view;
 
 use crate::structs::command::Command;
@@ -7,15 +8,20 @@ use slashook::{
     command,
     commands::{Command as SlashookCommand, CommandInput, CommandResponder},
     structs::{
+        Permissions,
         channels::ChannelType,
         interactions::{IntegrationType, InteractionContextType, InteractionOptionType},
-        Permissions,
     },
 };
 use std::sync::LazyLock;
 
-static COMMAND: LazyLock<Command> =
-    LazyLock::new(|| Command::new().subcommand("fix-embeds", fix_embeds::run).subcommand("logs", logs::run).subcommand("view", view::run));
+static COMMAND: LazyLock<Command> = LazyLock::new(|| {
+    Command::new()
+        .subcommand("fix-embeds", fix_embeds::run)
+        .subcommand("logs", logs::run)
+        .subcommand("prefix", prefix::run)
+        .subcommand("view", view::run)
+});
 
 pub fn get_command() -> SlashookCommand {
     #[command(
@@ -46,6 +52,20 @@ pub fn get_command() -> SlashookCommand {
 						description = "The channel to send logs to. Leave unset to disable logs entirely",
 						option_type = InteractionOptionType::CHANNEL,
                         channel_types = [ChannelType::GUILD_TEXT],
+                    },
+                ],
+            },
+            {
+                name = "prefix",
+                description = "Adds or removes a prefix.",
+                options = [
+					{
+						name = "prefix",
+						description = "The prefix to add or remove",
+						option_type = InteractionOptionType::STRING,
+                        max_length = 32,
+                        required = true,
+                        autocomplete = true,
                     },
                 ],
             },
---
+++ src/commands/utility/server_config/prefix.rs
@@ -0,0 +1,43 @@
+use std::cmp::Reverse;
+
+use crate::structs::{command_context::CommandContext, database::guilds::Guilds};
+use anyhow::Result;
+
+pub async fn run(ctx: CommandContext) -> Result<()> {
+    let mut guild = Guilds::get(ctx.input.guild_id.as_ref().unwrap()).await?;
+
+    if ctx.input.is_autocomplete() {
+        return ctx.autocomplete(guild.prefixes.iter().map(|prefix| (prefix, prefix))).await;
+    }
+
+    let prefix = ctx.get_string_arg("prefix")?.to_lowercase();
+    let remove_prefix = guild.prefixes.contains(&prefix);
+
+    if !remove_prefix && guild.prefixes.len() >= 10 {
+        return ctx.respond_error("A server can only have up to 10 prefixes.", true).await;
+    }
+
+    let message = if remove_prefix {
+        format!("Removed **{}** from prefixes.", prefix.replace("*", "\\*"))
+    } else {
+        format!("Added **{}** to prefixes.", prefix.replace("*", "\\*"))
+    };
+
+    if remove_prefix {
+        guild.prefixes = guild.prefixes.into_iter().filter(|entry| entry != &prefix).collect::<Vec<String>>();
+    } else {
+        guild.prefixes.push(prefix);
+
+        // Sort alphabetically
+        guild.prefixes.sort();
+
+        // Sort by longest to shortest for accuracy
+        guild.prefixes.sort_by_key(|entry| Reverse(entry.len()));
+    }
+
+    if let Err(error) = Guilds::update(guild).await {
+        ctx.respond_error(error, true).await
+    } else {
+        ctx.respond_success(message, true).await
+    }
+}
