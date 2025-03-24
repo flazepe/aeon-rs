191b47f5d3dcea19b4c3c50be48c4f49b0454e31
flazepe <flazepe@gmail.com>
2025-03-24 13:41:48 +0800

    refactor: use millis instead of secs

--- src/message_commands/utility/ping.rs
+++ src/message_commands/utility/ping.rs
@@ -6,5 +6,5 @@ use twilight_gateway::MessageSender;
 use twilight_model::channel::Message;
 
 pub async fn run<T: Display>(message: &Message, _sender: &MessageSender, _args: T) -> Result<()> {
-    message.send(format!("Pong! {}s", Utc::now().timestamp() - message.timestamp.as_secs())).await
+    message.send(format!("Pong! {}s", Utc::now().timestamp_millis() - message.timestamp.as_secs() * 1000)).await
 }
