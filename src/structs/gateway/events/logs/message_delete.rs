14c33e88dab6b5bed0c92df42d237b9060f52cfb
flazepe <flazepe@gmail.com>
2025-04-01 13:02:15 +0800

    feat: add deleted message link and creation date

--- src/structs/gateway/events/logs/message_delete.rs
+++ src/structs/gateway/events/logs/message_delete.rs
@@ -1,6 +1,7 @@
 use crate::{
+    functions::format_timestamp,
     statics::{CACHE, colors::ERROR_EMBED_COLOR},
-    structs::{database::guilds::Guilds, simple_message::SimpleMessage},
+    structs::{database::guilds::Guilds, simple_message::SimpleMessage, snowflake::Snowflake},
     traits::UserExt,
 };
 use anyhow::Result;
@@ -9,11 +10,21 @@ use twilight_model::gateway::payload::incoming::MessageDelete;
 
 pub async fn handle(event: &MessageDelete) -> Result<()> {
     let Some(guild_id) = event.guild_id else { return Ok(()) };
-    let mut embed = Embed::new().set_color(ERROR_EMBED_COLOR).unwrap_or_default().set_title("Message Deleted").add_field(
-        "Channel",
-        format!("<#{channel_id}> ({channel_id})", channel_id = event.channel_id),
-        false,
-    );
+
+    let snowflake = Snowflake::new(event.id)?;
+
+    let mut embed = Embed::new()
+        .set_color(ERROR_EMBED_COLOR)
+        .unwrap_or_default()
+        .set_title("Message Deleted")
+        .set_description(format!(
+            "https://discord.com/channels/{}/{}/{}",
+            event.guild_id.map(|guild_id| guild_id.to_string()).as_deref().unwrap_or(""),
+            event.channel_id,
+            event.id,
+        ))
+        .add_field("Channel", format!("<#{channel_id}> ({channel_id})", channel_id = event.channel_id), false)
+        .add_field("Created", format_timestamp(snowflake.timestamp.timestamp(), crate::functions::TimestampFormat::Full), false);
 
     let old_message = {
         let channels = CACHE.channels.read().unwrap();
