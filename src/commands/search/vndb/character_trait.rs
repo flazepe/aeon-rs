0d4073ca091ac0b06135f4ff79353ca209e19e4b
flazepe <flazepe@gmail.com>
2025-10-07 13:37:31 +0800

    feat: `/vndb trait` redesign

--- src/commands/search/vndb/character_trait.rs
+++ src/commands/search/vndb/character_trait.rs
@@ -4,19 +4,20 @@ use crate::structs::{
     select_menu::SelectMenu,
 };
 use anyhow::Result;
-use slashook::commands::MessageResponse;
 use std::sync::Arc;
 
 pub async fn run(ctx: Arc<AeonCommandContext>) -> Result<()> {
     if let AeonCommandInput::ApplicationCommand(input, _) = &ctx.command_input
         && input.is_string_select()
     {
-        return ctx.respond(Vndb::search_trait(&input.values.as_ref().unwrap()[0]).await?[0].format(), false).await;
+        return ctx
+            .respond(Vndb::search_trait(&input.values.as_ref().unwrap()[0]).await?[0].format().set_select_menu_from_input(input), false)
+            .await;
     }
 
     let results = Vndb::search_trait(ctx.get_string_arg("trait", 0, true)?).await?;
     let options = results.iter().map(|result| (&result.name, &result.id, result.group_name.as_ref()));
     let select_menu = SelectMenu::new("vndb", "trait", "View other traits…", None::<String>).add_options(options);
 
-    ctx.respond(MessageResponse::from(select_menu).add_embed(results[0].format()), false).await
+    ctx.respond(results[0].format().set_select_menu(select_menu), false).await
 }
