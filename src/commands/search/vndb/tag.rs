773a488331f6287e206c0c5038f16a922a961b12
flazepe <flazepe@gmail.com>
2025-10-07 13:29:27 +0800

    feat: `/vndb tag` redesign

--- src/commands/search/vndb/tag.rs
+++ src/commands/search/vndb/tag.rs
@@ -11,12 +11,13 @@ pub async fn run(ctx: Arc<AeonCommandContext>) -> Result<()> {
     if let AeonCommandInput::ApplicationCommand(input, _) = &ctx.command_input
         && input.is_string_select()
     {
-        return ctx.respond(Vndb::search_tag(&input.values.as_ref().unwrap()[0]).await?[0].format(), false).await;
+        let embed = Vndb::search_tag(&input.values.as_ref().unwrap()[0]).await?[0].format();
+        return ctx.respond(embed.set_select_menu_from_input(input), false).await;
     }
 
     let tags = Vndb::search_tag(ctx.get_string_arg("tag", 0, true)?).await?;
     let options = tags.iter().map(|tag| (&tag.name, &tag.id, Some(&tag.category)));
     let select_menu = SelectMenu::new("vndb", "tag", "View other tags…", None::<String>).add_options(options);
 
-    ctx.respond(MessageResponse::from(select_menu).add_embed(tags[0].format()), false).await
+    ctx.respond(MessageResponse::from(tags[0].format().set_select_menu(select_menu)), false).await
 }
