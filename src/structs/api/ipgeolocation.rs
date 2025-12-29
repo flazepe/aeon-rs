85f223b79676bbd1830cb5906d6833d6d893a989
flazepe <flazepe@gmail.com>
2025-12-29 10:27:41 +0800

    refactor: variables and formatting

--- src/structs/api/ipgeolocation.rs
+++ src/structs/api/ipgeolocation.rs
@@ -38,29 +38,28 @@ impl IPGeolocationTimeZone {
     }
 
     pub fn format(&self) -> String {
-        let mut time_split = self.time_zone.time_24.split(":");
-        let hour = time_split.next().unwrap_or_default().parse::<u8>().unwrap_or_default();
-        let min = time_split.next().unwrap_or_default();
-        let offset = if self.time_zone.is_dst { self.time_zone.offset_with_dst } else { self.time_zone.offset };
+        let location = &[self.location.city.as_str(), self.location.state_prov.as_str(), self.location.country_name.as_str()]
+            .into_iter()
+            .filter(|entry| !entry.is_empty())
+            .collect::<Vec<&str>>()
+            .join(", ");
 
+        let mut time_split = self.time_zone.time_24.split(":");
+        let hour_24 = time_split.next().unwrap_or_default().parse::<u8>().unwrap_or_default();
+        let hour_12 = if hour_24 == 0 || hour_24 == 12 { 12 } else { hour_24 % 12 };
+        let min = time_split.next().unwrap_or_default();
+        let am_pm = if hour_24 < 12 { "AM" } else { "PM" };
+
+        let date = self.time_zone.date_time_txt.split(" ").take(4).collect::<Vec<&str>>().join(" ");
+
+        let offset = if self.time_zone.is_dst { self.time_zone.offset_with_dst } else { self.time_zone.offset };
         let formatted_offset = format!(
             "UTC{}{:02}:{:02}",
             if offset >= 0.0 { "+" } else { "-" },
             offset.abs().trunc() as i32,
-            (offset.abs().fract() * 60.0).round() as i32
+            (offset.abs().fract() * 60.0).round() as i32,
         );
 
-        format!(
-            "It is `{}:{min} {}` or `{hour}:{min}` in {} (`{} / {}`).",
-            if hour == 0 || hour == 12 { 12 } else { hour % 12 },
-            if hour < 12 { "AM" } else { "PM" },
-            &[self.location.city.as_str(), self.location.state_prov.as_str(), self.location.country_name.as_str(),]
-                .into_iter()
-                .filter(|entry| !entry.is_empty())
-                .collect::<Vec<&str>>()
-                .join(", "),
-            self.time_zone.date_time_txt.split(" ").take(4).collect::<Vec<&str>>().join(" "),
-            formatted_offset,
-        )
+        format!("It is `{hour_12}:{min} {am_pm}` or `{hour_24}:{min}` in {location} (`{date} / {formatted_offset}`).")
     }
 }
