7e4ffee6243a14aca729fadf697473febddd8c04
flazepe <flazepe@gmail.com>
2026-09-12 19:36:21 +0800

    feat: show all dictionary entries

--- src/structs/api/dictionary.rs
+++ src/structs/api/dictionary.rs
@@ -1,6 +1,10 @@
-use crate::{functions::limit_strings, statics::REQWEST};
+use crate::{
+    functions::limit_strings,
+    statics::{REQWEST, colors::PRIMARY_EMBED_COLOR},
+};
 use anyhow::{Error, Result};
 use serde::Deserialize;
+use slashook::structs::embeds::Embed;
 use std::fmt::Display;
 
 #[derive(Deserialize, Debug)]
@@ -13,11 +17,11 @@ pub struct Dictionary {
 #[serde(rename_all = "camelCase")]
 pub struct DictionaryEntry {
     pub part_of_speech: String,
-    pub senses: Vec<DictionaryDefinition>,
+    pub senses: Vec<DictionarySense>,
 }
 
 #[derive(Deserialize, Debug)]
-pub struct DictionaryDefinition {
+pub struct DictionarySense {
     pub definition: String,
 }
 
@@ -32,14 +36,17 @@ impl Dictionary {
             .map_err(|_| Error::msg("Word not found."))
     }
 
-    pub fn format(&self) -> String {
-        let entry = &self.entries[0];
-
-        format!(
-            "# {}\n[{}]\n{}",
-            self.word,
-            entry.part_of_speech,
-            limit_strings(entry.senses.iter().map(|meaning| format!("- {}", meaning.definition)), "\n", 1900),
-        )
+    pub fn format(&self) -> Embed {
+        Embed::new().set_color(PRIMARY_EMBED_COLOR).unwrap_or_default().set_title(&self.word).set_description(limit_strings(
+            self.entries.iter().map(|entry| {
+                format!(
+                    "-# {}\n{}",
+                    entry.part_of_speech,
+                    entry.senses.iter().map(|sense| format!("- {}", sense.definition)).collect::<Vec<String>>().join("\n"),
+                )
+            }),
+            "\n",
+            4096,
+        ))
     }
 }
